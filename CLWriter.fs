275e25f391842e0d836518a1ac78075289a41d15
Evelyn Borth <borth@rhrk.uni-kl.de>
2023-01-07 01:21:03 +0100

    CommonWriter

--- CLWriter.fs
+++ CLWriter.fs
@@ -1,75 +1,11 @@
 module Translator.CLWriter
 
+open Translator.CommonWriter
 open Translator.PathAnalyzer
 open Averest.MiniC.DataflowProcessNetworks
-open Averest.MiniC.Types
-open System
-open System.Collections.Generic
+
 
 let tabSpace = "    "
-
-let generateMethodCode dpn (bufferTypes : Map<string, string>) path buffers name : string =
-    let (input, output) = buffers
-
-    // write signature
-    let mutable code = "void " + name + "("
-    input |> List.iter (fun x -> code <- code + bufferTypes[x] + " " + x + ", ")
-    output |> List.iter (fun x -> code <- code + bufferTypes[x] + "* " + x + ", ")
-    code <- code[0 .. code.Length - 3] + ") { \n"
-
-    // write body
-
-    // helpers
-    let formatWriteVariable name =
-        if  List.contains name output then
-            "*" + name
-        else
-            bufferTypes[name] + " " + name
-
-    let writeBodyInst str =
-        code <- code + tabSpace + str + "\n"
-
-    let writeAssign varName value = 
-        writeBodyInst ((formatWriteVariable varName) + " = " + value + ";")
-
-    let writeAssignFormula out v1 op v2 =
-        writeBodyInst ((formatWriteVariable out) + " = " + (v1 + op + v2) + ";")
-
-
-    for node in path do
-        let (outA, dp, inA) = dpn.nodes[node]
-        match dp with
-        | Copy -> writeAssign outA[0] inA[0]
-        | Dup -> 
-            writeAssign outA[0] inA[0]
-            writeAssign outA[1] inA[0]
-        | BinOp op -> 
-            let sign = 
-                match op with
-                | MulN | MulZ -> " * "
-                | AddN | AddZ -> " + "
-                | SubN | SubZ -> " - "
-                | DivN | DivZ -> " / "
-                | ModN | ModZ -> " % "
-                | AndB -> " && "
-                | OrB -> " || "
-                | LesN | LesZ -> " < "
-                | LeqN | LeqZ -> " <= "
-                | EqqN | EqqZ | EqqB -> " == "
-                | NeqN | NeqZ | NeqB -> " != "
-
-            writeAssignFormula outA[0] inA[0] sign inA[1]
-
-        | MonOp op ->
-            match op with
-            | NotB -> writeAssign outA[0] ("!" + inA[0])
-            | CastB -> writeAssign outA[0] ("(bool)" + inA[0])
-            | CastN | CastZ -> writeAssign outA[0] ("(int)" + inA[0])
-        | Const value -> writeAssign outA[0] (value.ToString())
-        | _ -> raise (Exception "Undefined node in path")
-
-    code <- code + "}\n"
-    code
 
 let generateCLCode dpn (bufferTypes : Map<string, string>) (pathsAndBuffers : Set<(int list) * PathBuffers>) : string =
     let mutable code = ""
--- OpenMPWriter.fs
+++ OpenMPWriter.fs
@@ -1,76 +1,12 @@
 module Translator.OpenMPWriter
 open Translator.PathAnalyzer
 open Averest.MiniC.DataflowProcessNetworks
-open Averest.MiniC.Types
-open System
+open Translator.CommonWriter
 
 let tabSpace = "    "
 let tabSpace2 = "       "
 let tabSpace3 = "           "
 let tabSpace4 = "               "
-
-let generateMethodCode dpn (bufferTypes : Map<string, string>) path buffers name : string =
-    let (input, output) = buffers
-
-    // write signature
-    let mutable code = "void " + name + "("
-    input |> List.iter (fun x -> code <- code + bufferTypes[x] + " " + x + ", ")
-    output |> List.iter (fun x -> code <- code + bufferTypes[x] + "* " + x + ", ")
-    code <- code[0 .. code.Length - 3] + ") { \n"
-
-    // write body
-
-    // helpers
-    let formatWriteVariable name =
-        if  List.contains name output then
-            "*" + name
-        else
-            bufferTypes[name] + " " + name
-
-    let writeBodyInst str =
-        code <- code + tabSpace + str + "\n"
-
-    let writeAssign varName value = 
-        writeBodyInst ((formatWriteVariable varName) + " = " + value + ";")
-
-    let writeAssignFormula out v1 op v2 =
-        writeBodyInst ((formatWriteVariable out) + " = " + (v1 + op + v2) + ";")
-
-
-    for node in path do
-        let (outA, dp, inA) = dpn.nodes[node]
-        match dp with
-        | Copy -> writeAssign outA[0] inA[0]
-        | Dup -> 
-            writeAssign outA[0] inA[0]
-            writeAssign outA[1] inA[0]
-        | BinOp op -> 
-            let sign = 
-                match op with
-                | MulN | MulZ -> " * "
-                | AddN | AddZ -> " + "
-                | SubN | SubZ -> " - "
-                | DivN | DivZ -> " / "
-                | ModN | ModZ -> " % "
-                | AndB -> " && "
-                | OrB -> " || "
-                | LesN | LesZ -> " < "
-                | LeqN | LeqZ -> " <= "
-                | EqqN | EqqZ | EqqB -> " == "
-                | NeqN | NeqZ | NeqB -> " != "
-
-            writeAssignFormula outA[0] inA[0] sign inA[1]
-
-        | MonOp op ->
-            match op with
-            | NotB -> writeAssign outA[0] ("!" + inA[0])
-            | CastB -> writeAssign outA[0] ("(bool)" + inA[0])
-            | CastN | CastZ -> writeAssign outA[0] ("(int)" + inA[0])
-        | Const value -> writeAssign outA[0] (value.ToString())
-        | _ -> raise (Exception "Undefined node in path")
-
-    code <- code + "}\n"
-    code
 
 let generateOpenMPCode dpn (bufferTypes : Map<string, string>) (pathsAndBuffers : Set<(int list) * PathBuffers>) : string =
     let mutable code = ""
@@ -81,7 +17,8 @@ let generateOpenMPCode dpn (bufferTypes : Map<string, string>) (pathsAndBuffers 
     let datasize = int32 (System.Console.ReadLine())
     printfn "Enter input:"
     let input = System.Console.ReadLine()
-           
+    let inputArray = input.Split ','
+   
     code <- sprintf "%s#include<stdio.h>\n#include<omp.h>\n#define DATA_SIZE %d\n\n" code datasize
 
     for pathBuffer in pathsAndBuffers do
@@ -90,6 +27,7 @@ let generateOpenMPCode dpn (bufferTypes : Map<string, string>) (pathsAndBuffers 
         count <- count + 1
     
     count <- 0
+    code <- sprintf "%sauto start = chrono::steady_clock::now();" code
     code <- sprintf "%sint main() { \n%sint inputs[] = {%s} \n%sint outputs = [DATA_SIZE * 2]\n" code tabSpace input tabSpace
            
     let paths = pathsAndBuffers |> Set.toList
@@ -141,8 +79,22 @@ let generateOpenMPCode dpn (bufferTypes : Map<string, string>) (pathsAndBuffers 
     count <- 0
     dpn.outVars |> Set.iter (fun x -> 
         code  <- sprintf"%s%soutputs[i * %d + %d] = %s;\n"code  tabSpace2  dpn.outVars.Count  count  x 
-        count <- count + 1)
+        count <- count + 1
+        )
     code <- sprintf "%s%s}\n"code tabSpace
-    code <- sprintf "%sprintf(b: %%i, c: %%i, outputs[0], outputs[1]);\n" code
-    code <- sprintf "%sprintf(b: %%i, c: %%i, outputs[2], outputs[3]);\n}" code
+
+    count <- 0
+    while(count < datasize * inputArray.Length) do
+        code <-sprintf "%sprintf(" code
+        for out in dpn.outVars do
+            code <- sprintf "%s%s: %%i, " code out
+        code <- code[0 ..  code.Length - 3] + ","
+        for out in dpn.outVars do
+            code <- sprintf "%soutputs[%d]," code count
+        code <- code[0 ..  code.Length - 3] + ");"
+
+    code <- code[0 ..  code.Length - 3] + ");\n"
+    code <- sprintf "%sauto end = chrono::steady_clock::now();\nprintf(\"time: %%i\", (end - start).count());" code
     code
+    
+    
